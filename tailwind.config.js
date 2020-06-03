4cd51c9651e1be2869c6e50def6c8460b9662bad
Abdirahmanyusuf <59662359+Abdirahmanyusuf@users.noreply.github.com>
2020-06-04 01:10:01 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+
+    },
+  },
+  LineHeight: {
+    'extra-loose': '2.5',
+  },
+  spacing: {
+    '200': '50rem',
+  },
+  variants: {},
+  plugins: [],
+}
